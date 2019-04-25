PriyamDutta/RxSwift

Rename C generic constraint to Collection

RxCocoa/Traits/SharedSequence/SharedSequence+Operators.swift
@@ -226,8 +226,8 @@ extension SharedSequenceConvertibleType {
      - parameter sources: Collection of observable sequences to merge.
      - returns: The observable sequence that merges the elements of the observable sequences.
      */
-    public static func merge<C: Collection>(_ sources: C) -> SharedSequence<SharingStrategy, Element>
-        where C.Iterator.Element == SharedSequence<SharingStrategy, Element> {
+    public static func merge<Collection: Swift.Collection>(_ sources: Collection) -> SharedSequence<SharingStrategy, Element>
+        where Collection.Element == SharedSequence<SharingStrategy, Element> {
         let source = Observable.merge(sources.map { $0.asObservable() })
         return SharedSequence<SharingStrategy, Element>(source)
     }
@@ -364,8 +364,8 @@ extension SharedSequence {
 
      - returns: An observable sequence that contains the elements of each given sequence, in sequential order.
      */
-    public static func concat<C: Collection>(_ collection: C) -> SharedSequence<SharingStrategy, Element>
-        where C.Iterator.Element == SharedSequence<SharingStrategy, Element> {
+    public static func concat<Collection: Swift.Collection>(_ collection: Collection) -> SharedSequence<SharingStrategy, Element>
+        where Collection.Element == SharedSequence<SharingStrategy, Element> {
         let source = Observable.concat(collection.map { $0.asObservable() })
         return SharedSequence<SharingStrategy, Element>(source)
     }
@@ -380,8 +380,8 @@ extension SharedSequence {
      - parameter resultSelector: Function to invoke for each series of elements at corresponding indexes in the sources.
      - returns: An observable sequence containing the result of combining elements of the sources using the specified result selector function.
      */
-    public static func zip<C: Collection, R>(_ collection: C, resultSelector: @escaping ([Element]) throws -> R) -> SharedSequence<SharingStrategy, R>
-        where C.Iterator.Element == SharedSequence<SharingStrategy, Element> {
+    public static func zip<Collection: Swift.Collection, R>(_ collection: Collection, resultSelector: @escaping ([Element]) throws -> R) -> SharedSequence<SharingStrategy, R>
+        where Collection.Element == SharedSequence<SharingStrategy, Element> {
         let source = Observable.zip(collection.map { $0.asSharedSequence().asObservable() }, resultSelector: resultSelector)
         return SharedSequence<SharingStrategy, R>(source)
     }
@@ -391,8 +391,8 @@ extension SharedSequence {
 
      - returns: An observable sequence containing the result of combining elements of the sources.
      */
-    public static func zip<C: Collection>(_ collection: C) -> SharedSequence<SharingStrategy, [Element]>
-        where C.Iterator.Element == SharedSequence<SharingStrategy, Element> {
+    public static func zip<Collection: Swift.Collection>(_ collection: Collection) -> SharedSequence<SharingStrategy, [Element]>
+        where Collection.Element == SharedSequence<SharingStrategy, Element> {
             let source = Observable.zip(collection.map { $0.asSharedSequence().asObservable() })
             return SharedSequence<SharingStrategy, [Element]>(source)
     }
@@ -407,8 +407,8 @@ extension SharedSequence {
      - parameter resultSelector: Function to invoke whenever any of the sources produces an element.
      - returns: An observable sequence containing the result of combining elements of the sources using the specified result selector function.
      */
-    public static func combineLatest<C: Collection, R>(_ collection: C, resultSelector: @escaping ([Element]) throws -> R) -> SharedSequence<SharingStrategy, R>
-        where C.Iterator.Element == SharedSequence<SharingStrategy, Element> {
+    public static func combineLatest<Collection: Swift.Collection, R>(_ collection: Collection, resultSelector: @escaping ([Element]) throws -> R) -> SharedSequence<SharingStrategy, R>
+        where Collection.Element == SharedSequence<SharingStrategy, Element> {
         let source = Observable.combineLatest(collection.map { $0.asObservable() }, resultSelector: resultSelector)
         return SharedSequence<SharingStrategy, R>(source)
     }
@@ -418,8 +418,8 @@ extension SharedSequence {
 
      - returns: An observable sequence containing the result of combining elements of the sources.
      */
-    public static func combineLatest<C: Collection>(_ collection: C) -> SharedSequence<SharingStrategy, [Element]>
-        where C.Iterator.Element == SharedSequence<SharingStrategy, Element> {
+    public static func combineLatest<Collection: Swift.Collection>(_ collection: Collection) -> SharedSequence<SharingStrategy, [Element]>
+        where Collection.Element == SharedSequence<SharingStrategy, Element> {
         let source = Observable.combineLatest(collection.map { $0.asObservable() })
         return SharedSequence<SharingStrategy, [Element]>(source)
     }

RxSwift/Observables/CombineLatest+Collection.swift
@@ -15,8 +15,8 @@ extension ObservableType {
      - parameter resultSelector: Function to invoke whenever any of the sources produces an element.
      - returns: An observable sequence containing the result of combining elements of the sources using the specified result selector function.
      */
-    public static func combineLatest<C: Collection>(_ collection: C, resultSelector: @escaping ([C.Iterator.Element.Element]) throws -> Element) -> Observable<Element>
-        where C.Iterator.Element: ObservableType {
+    public static func combineLatest<Collection: Swift.Collection>(_ collection: Collection, resultSelector: @escaping ([Collection.Element.Element]) throws -> Element) -> Observable<Element>
+        where Collection.Element: ObservableType {
         return CombineLatestCollectionType(sources: collection, resultSelector: resultSelector)
     }
 
@@ -27,17 +27,17 @@ extension ObservableType {
 
      - returns: An observable sequence containing the result of combining elements of the sources.
      */
-    public static func combineLatest<C: Collection>(_ collection: C) -> Observable<[Element]>
-        where C.Iterator.Element: ObservableType, C.Iterator.Element.Element == Element {
+    public static func combineLatest<Collection: Swift.Collection>(_ collection: Collection) -> Observable<[Element]>
+        where Collection.Element: ObservableType, Collection.Element.Element == Element {
         return CombineLatestCollectionType(sources: collection, resultSelector: { $0 })
     }
 }
 
-final private class CombineLatestCollectionTypeSink<C: Collection, O: ObserverType>
-    : Sink<O> where C.Iterator.Element: ObservableConvertibleType {
+final private class CombineLatestCollectionTypeSink<Collection: Swift.Collection, O: ObserverType>
+    : Sink<O> where Collection.Element: ObservableConvertibleType {
     typealias R = O.Element 
-    typealias Parent = CombineLatestCollectionType<C, R>
-    typealias SourceElement = C.Iterator.Element.Element
+    typealias Parent = CombineLatestCollectionType<Collection, R>
+    typealias SourceElement = Collection.Element.Element
     
     let _parent: Parent
     
@@ -145,14 +145,14 @@ final private class CombineLatestCollectionTypeSink<C: Collection, O: ObserverTy
     }
 }
 
-final private class CombineLatestCollectionType<C: Collection, R>: Producer<R> where C.Iterator.Element: ObservableConvertibleType {
-    typealias ResultSelector = ([C.Iterator.Element.Element]) throws -> R
+final private class CombineLatestCollectionType<Collection: Swift.Collection, R>: Producer<R> where Collection.Element: ObservableConvertibleType {
+    typealias ResultSelector = ([Collection.Element.Element]) throws -> R
     
-    let _sources: C
+    let _sources: Collection
     let _resultSelector: ResultSelector
     let _count: Int
 
-    init(sources: C, resultSelector: @escaping ResultSelector) {
+    init(sources: Collection, resultSelector: @escaping ResultSelector) {
         self._sources = sources
         self._resultSelector = resultSelector
         self._count = Int(Int64(self._sources.count))

RxSwift/Observables/Zip+Collection.swift
@@ -15,8 +15,8 @@ extension ObservableType {
      - parameter resultSelector: Function to invoke for each series of elements at corresponding indexes in the sources.
      - returns: An observable sequence containing the result of combining elements of the sources using the specified result selector function.
      */
-    public static func zip<C: Collection>(_ collection: C, resultSelector: @escaping ([C.Iterator.Element.Element]) throws -> Element) -> Observable<Element>
-        where C.Iterator.Element: ObservableType {
+    public static func zip<Collection: Swift.Collection>(_ collection: Collection, resultSelector: @escaping ([Collection.Element.Element]) throws -> Element) -> Observable<Element>
+        where Collection.Element: ObservableType {
         return ZipCollectionType(sources: collection, resultSelector: resultSelector)
     }
 
@@ -27,18 +27,18 @@ extension ObservableType {
 
      - returns: An observable sequence containing the result of combining elements of the sources.
      */
-    public static func zip<C: Collection>(_ collection: C) -> Observable<[Element]>
-        where C.Iterator.Element: ObservableType, C.Iterator.Element.Element == Element {
+    public static func zip<Collection: Swift.Collection>(_ collection: Collection) -> Observable<[Element]>
+        where Collection.Element: ObservableType, Collection.Element.Element == Element {
         return ZipCollectionType(sources: collection, resultSelector: { $0 })
     }
     
 }
 
-final private class ZipCollectionTypeSink<C: Collection, O: ObserverType>
-    : Sink<O> where C.Iterator.Element: ObservableConvertibleType {
+final private class ZipCollectionTypeSink<Collection: Swift.Collection, O: ObserverType>
+    : Sink<O> where Collection.Element: ObservableConvertibleType {
     typealias R = O.Element 
-    typealias Parent = ZipCollectionType<C, R>
-    typealias SourceElement = C.Iterator.Element.Element
+    typealias Parent = ZipCollectionType<Collection, R>
+    typealias SourceElement = Collection.Element.Element
     
     private let _parent: Parent
     
@@ -148,14 +148,14 @@ final private class ZipCollectionTypeSink<C: Collection, O: ObserverType>
     }
 }
 
-final private class ZipCollectionType<C: Collection, R>: Producer<R> where C.Iterator.Element: ObservableConvertibleType {
-    typealias ResultSelector = ([C.Iterator.Element.Element]) throws -> R
+final private class ZipCollectionType<Collection: Swift.Collection, R>: Producer<R> where Collection.Element: ObservableConvertibleType {
+    typealias ResultSelector = ([Collection.Element.Element]) throws -> R
     
-    let sources: C
+    let sources: Collection
     let resultSelector: ResultSelector
     let count: Int
     
-    init(sources: C, resultSelector: @escaping ResultSelector) {
+    init(sources: Collection, resultSelector: @escaping ResultSelector) {
         self.sources = sources
         self.resultSelector = resultSelector
         self.count = Int(Int64(self.sources.count))

RxSwift/Traits/Single.swift
@@ -258,7 +258,7 @@ extension PrimitiveSequenceType where Trait == SingleTrait {
      - parameter resultSelector: Function to invoke for each series of elements at corresponding indexes in the sources.
      - returns: An observable sequence containing the result of combining elements of the sources using the specified result selector function.
      */
-    public static func zip<C: Collection, R>(_ collection: C, resultSelector: @escaping ([Element]) throws -> R) -> PrimitiveSequence<Trait, R> where C.Iterator.Element == PrimitiveSequence<Trait, Element> {
+    public static func zip<Collection: Swift.Collection, R>(_ collection: Collection, resultSelector: @escaping ([Element]) throws -> R) -> PrimitiveSequence<Trait, R> where Collection.Element == PrimitiveSequence<Trait, Element> {
         
         if collection.isEmpty {
             return PrimitiveSequence<Trait, R>.deferred {
@@ -275,7 +275,7 @@ extension PrimitiveSequenceType where Trait == SingleTrait {
      
      - returns: An observable sequence containing the result of combining elements of the sources.
      */
-    public static func zip<C: Collection>(_ collection: C) -> PrimitiveSequence<Trait, [Element]> where C.Iterator.Element == PrimitiveSequence<Trait, Element> {
+    public static func zip<Collection: Swift.Collection>(_ collection: Collection) -> PrimitiveSequence<Trait, [Element]> where Collection.Element == PrimitiveSequence<Trait, Element> {
         
         if collection.isEmpty {
             return PrimitiveSequence<Trait, [Element]>(raw: .just([]))

Serhey-Oleynik/mxswift

RxCocoa/Traits/SharedSequence/SharedSequence+Operators.swift
@@ -226,8 +226,8 @@ extension SharedSequenceConvertibleType {
      - parameter sources: Collection of observable sequences to merge.
      - returns: The observable sequence that merges the elements of the observable sequences.
      */
-    public static func merge<C: Collection>(_ sources: C) -> SharedSequence<SharingStrategy, Element>
-        where C.Iterator.Element == SharedSequence<SharingStrategy, Element> {
+    public static func merge<Collection: Swift.Collection>(_ sources: Collection) -> SharedSequence<SharingStrategy, Element>
+        where Collection.Element == SharedSequence<SharingStrategy, Element> {
         let source = Observable.merge(sources.map { $0.asObservable() })
         return SharedSequence<SharingStrategy, Element>(source)
     }
@@ -364,8 +364,8 @@ extension SharedSequence {
 
      - returns: An observable sequence that contains the elements of each given sequence, in sequential order.
      */
-    public static func concat<C: Collection>(_ collection: C) -> SharedSequence<SharingStrategy, Element>
-        where C.Iterator.Element == SharedSequence<SharingStrategy, Element> {
+    public static func concat<Collection: Swift.Collection>(_ collection: Collection) -> SharedSequence<SharingStrategy, Element>
+        where Collection.Element == SharedSequence<SharingStrategy, Element> {
         let source = Observable.concat(collection.map { $0.asObservable() })
         return SharedSequence<SharingStrategy, Element>(source)
     }
@@ -380,8 +380,8 @@ extension SharedSequence {
      - parameter resultSelector: Function to invoke for each series of elements at corresponding indexes in the sources.
      - returns: An observable sequence containing the result of combining elements of the sources using the specified result selector function.
      */
-    public static func zip<C: Collection, Result>(_ collection: C, resultSelector: @escaping ([Element]) throws -> Result) -> SharedSequence<SharingStrategy, Result>
-        where C.Iterator.Element == SharedSequence<SharingStrategy, Element> {
+    public static func zip<Collection: Swift.Collection, Result>(_ collection: Collection, resultSelector: @escaping ([Element]) throws -> Result) -> SharedSequence<SharingStrategy, Result>
+        where Collection.Element == SharedSequence<SharingStrategy, Element> {
         let source = Observable.zip(collection.map { $0.asSharedSequence().asObservable() }, resultSelector: resultSelector)
         return SharedSequence<SharingStrategy, Result>(source)
     }
@@ -391,8 +391,8 @@ extension SharedSequence {
 
      - returns: An observable sequence containing the result of combining elements of the sources.
      */
-    public static func zip<C: Collection>(_ collection: C) -> SharedSequence<SharingStrategy, [Element]>
-        where C.Iterator.Element == SharedSequence<SharingStrategy, Element> {
+    public static func zip<Collection: Swift.Collection>(_ collection: Collection) -> SharedSequence<SharingStrategy, [Element]>
+        where Collection.Element == SharedSequence<SharingStrategy, Element> {
             let source = Observable.zip(collection.map { $0.asSharedSequence().asObservable() })
             return SharedSequence<SharingStrategy, [Element]>(source)
     }
@@ -407,8 +407,8 @@ extension SharedSequence {
      - parameter resultSelector: Function to invoke whenever any of the sources produces an element.
      - returns: An observable sequence containing the result of combining elements of the sources using the specified result selector function.
      */
-    public static func combineLatest<C: Collection, Result>(_ collection: C, resultSelector: @escaping ([Element]) throws -> Result) -> SharedSequence<SharingStrategy, Result>
-        where C.Iterator.Element == SharedSequence<SharingStrategy, Element> {
+    public static func combineLatest<Collection: Swift.Collection, Result>(_ collection: Collection, resultSelector: @escaping ([Element]) throws -> Result) -> SharedSequence<SharingStrategy, Result>
+        where Collection.Element == SharedSequence<SharingStrategy, Element> {
         let source = Observable.combineLatest(collection.map { $0.asObservable() }, resultSelector: resultSelector)
         return SharedSequence<SharingStrategy, Result>(source)
     }
@@ -418,8 +418,8 @@ extension SharedSequence {
 
      - returns: An observable sequence containing the result of combining elements of the sources.
      */
-    public static func combineLatest<C: Collection>(_ collection: C) -> SharedSequence<SharingStrategy, [Element]>
-        where C.Iterator.Element == SharedSequence<SharingStrategy, Element> {
+    public static func combineLatest<Collection: Swift.Collection>(_ collection: Collection) -> SharedSequence<SharingStrategy, [Element]>
+        where Collection.Element == SharedSequence<SharingStrategy, Element> {
         let source = Observable.combineLatest(collection.map { $0.asObservable() })
         return SharedSequence<SharingStrategy, [Element]>(source)
     }

RxSwift/Observables/CombineLatest+Collection.swift
@@ -15,8 +15,8 @@ extension ObservableType {
      - parameter resultSelector: Function to invoke whenever any of the sources produces an element.
      - returns: An observable sequence containing the result of combining elements of the sources using the specified result selector function.
      */
-    public static func combineLatest<C: Collection>(_ collection: C, resultSelector: @escaping ([C.Iterator.Element.Element]) throws -> Element) -> Observable<Element>
-        where C.Iterator.Element: ObservableType {
+    public static func combineLatest<Collection: Swift.Collection>(_ collection: Collection, resultSelector: @escaping ([Collection.Element.Element]) throws -> Element) -> Observable<Element>
+        where Collection.Element: ObservableType {
         return CombineLatestCollectionType(sources: collection, resultSelector: resultSelector)
     }
 
@@ -27,17 +27,17 @@ extension ObservableType {
 
      - returns: An observable sequence containing the result of combining elements of the sources.
      */
-    public static func combineLatest<C: Collection>(_ collection: C) -> Observable<[Element]>
-        where C.Iterator.Element: ObservableType, C.Iterator.Element.Element == Element {
+    public static func combineLatest<Collection: Swift.Collection>(_ collection: Collection) -> Observable<[Element]>
+        where Collection.Element: ObservableType, Collection.Element.Element == Element {
         return CombineLatestCollectionType(sources: collection, resultSelector: { $0 })
     }
 }
 
-final private class CombineLatestCollectionTypeSink<C: Collection, O: ObserverType>
-    : Sink<O> where C.Iterator.Element: ObservableConvertibleType {
-    typealias Result = O.Element
-    typealias Parent = CombineLatestCollectionType<C, Result>
-    typealias SourceElement = C.Iterator.Element.Element
+final private class CombineLatestCollectionTypeSink<Collection: Swift.Collection, O: ObserverType>
+    : Sink<O> where Collection.Element: ObservableConvertibleType {
+    typealias Result = O.Element 
+    typealias Parent = CombineLatestCollectionType<Collection, Result>
+    typealias SourceElement = Collection.Element.Element
     
     let _parent: Parent
     
@@ -145,14 +145,14 @@ final private class CombineLatestCollectionTypeSink<C: Collection, O: ObserverTy
     }
 }
 
-final private class CombineLatestCollectionType<C: Collection, Result>: Producer<Result> where C.Iterator.Element: ObservableConvertibleType {
-    typealias ResultSelector = ([C.Iterator.Element.Element]) throws -> Result
+final private class CombineLatestCollectionType<Collection: Swift.Collection, Result>: Producer<Result> where Collection.Element: ObservableConvertibleType {
+    typealias ResultSelector = ([Collection.Element.Element]) throws -> Result
     
-    let _sources: C
+    let _sources: Collection
     let _resultSelector: ResultSelector
     let _count: Int
 
-    init(sources: C, resultSelector: @escaping ResultSelector) {
+    init(sources: Collection, resultSelector: @escaping ResultSelector) {
         self._sources = sources
         self._resultSelector = resultSelector
         self._count = Int(Int64(self._sources.count))

RxSwift/Observables/Zip+Collection.swift
@@ -15,8 +15,8 @@ extension ObservableType {
      - parameter resultSelector: Function to invoke for each series of elements at corresponding indexes in the sources.
      - returns: An observable sequence containing the result of combining elements of the sources using the specified result selector function.
      */
-    public static func zip<C: Collection>(_ collection: C, resultSelector: @escaping ([C.Iterator.Element.Element]) throws -> Element) -> Observable<Element>
-        where C.Iterator.Element: ObservableType {
+    public static func zip<Collection: Swift.Collection>(_ collection: Collection, resultSelector: @escaping ([Collection.Element.Element]) throws -> Element) -> Observable<Element>
+        where Collection.Element: ObservableType {
         return ZipCollectionType(sources: collection, resultSelector: resultSelector)
     }
 
@@ -27,18 +27,18 @@ extension ObservableType {
 
      - returns: An observable sequence containing the result of combining elements of the sources.
      */
-    public static func zip<C: Collection>(_ collection: C) -> Observable<[Element]>
-        where C.Iterator.Element: ObservableType, C.Iterator.Element.Element == Element {
+    public static func zip<Collection: Swift.Collection>(_ collection: Collection) -> Observable<[Element]>
+        where Collection.Element: ObservableType, Collection.Element.Element == Element {
         return ZipCollectionType(sources: collection, resultSelector: { $0 })
     }
     
 }
 
-final private class ZipCollectionTypeSink<C: Collection, O: ObserverType>
-    : Sink<O> where C.Iterator.Element: ObservableConvertibleType {
+final private class ZipCollectionTypeSink<Collection: Swift.Collection, O: ObserverType>
+    : Sink<O> where Collection.Element: ObservableConvertibleType {
     typealias Result = O.Element 
-    typealias Parent = ZipCollectionType<C, Result>
-    typealias SourceElement = C.Iterator.Element.Element
+    typealias Parent = ZipCollectionType<Collection, Result>
+    typealias SourceElement = Collection.Element.Element
     
     private let _parent: Parent
     
@@ -148,14 +148,14 @@ final private class ZipCollectionTypeSink<C: Collection, O: ObserverType>
     }
 }
 
-final private class ZipCollectionType<C: Collection, Result>: Producer<Result> where C.Iterator.Element: ObservableConvertibleType {
-    typealias ResultSelector = ([C.Iterator.Element.Element]) throws -> Result
+final private class ZipCollectionType<Collection: Swift.Collection, Result>: Producer<Result> where Collection.Element: ObservableConvertibleType {
+    typealias ResultSelector = ([Collection.Element.Element]) throws -> Result
     
-    let sources: C
+    let sources: Collection
     let resultSelector: ResultSelector
     let count: Int
     
-    init(sources: C, resultSelector: @escaping ResultSelector) {
+    init(sources: Collection, resultSelector: @escaping ResultSelector) {
         self.sources = sources
         self.resultSelector = resultSelector
         self.count = Int(Int64(self.sources.count))

RxSwift/Traits/Single.swift
@@ -258,7 +258,7 @@ extension PrimitiveSequenceType where Trait == SingleTrait {
      - parameter resultSelector: Function to invoke for each series of elements at corresponding indexes in the sources.
      - returns: An observable sequence containing the result of combining elements of the sources using the specified result selector function.
      */
-    public static func zip<C: Collection, Result>(_ collection: C, resultSelector: @escaping ([Element]) throws -> Result) -> PrimitiveSequence<Trait, Result> where C.Iterator.Element == PrimitiveSequence<Trait, Element> {
+    public static func zip<Collection: Swift.Collection, Result>(_ collection: Collection, resultSelector: @escaping ([Element]) throws -> Result) -> PrimitiveSequence<Trait, Result> where Collection.Element == PrimitiveSequence<Trait, Element> {
         
         if collection.isEmpty {
             return PrimitiveSequence<Trait, Result>.deferred {
@@ -275,7 +275,7 @@ extension PrimitiveSequenceType where Trait == SingleTrait {
      
      - returns: An observable sequence containing the result of combining elements of the sources.
      */
-    public static func zip<C: Collection>(_ collection: C) -> PrimitiveSequence<Trait, [Element]> where C.Iterator.Element == PrimitiveSequence<Trait, Element> {
+    public static func zip<Collection: Swift.Collection>(_ collection: Collection) -> PrimitiveSequence<Trait, [Element]> where Collection.Element == PrimitiveSequence<Trait, Element> {
         
         if collection.isEmpty {
             return PrimitiveSequence<Trait, [Element]>(raw: .just([]))